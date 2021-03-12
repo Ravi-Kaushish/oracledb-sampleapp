1a3c5c59c6659dc065cee2f1b70c942bf246d906
Ravi-Kaushish <ravikantsharma2169@gmail.com>
2021-03-12 15:01:31 +0530

    added insert query sample

--- controllers/test.js
+++ controllers/test.js
@@ -1,7 +1,7 @@
 const oracledb = require('oracledb');
 
 try {
-  oracledb.initOracleClient({libDir: 'C:/Users/ravikant.sharma/Desktop/oracle/instantclient_19_10'});
+  oracledb.initOracleClient({libDir: 'C:/oracle/product/instantclient_19_10'});
 } catch (err) {
   console.error('Whoops!');
   console.error(err);
@@ -13,13 +13,25 @@ exports.TestFuxn = async (req, res) => {
   let connection;
   try {
    connection = await oracledb.getConnection({ user: "ADMIN", password: "IgniteDB1234", connectionString: "igniteoracledb_high" });
-
+    // console.log(connection)
     // Create a table
-    let result = await connection.execute(`CREATE TABLE myusers5 (id number, name varchar2(20), age number)`);
-    // let result = await connection.execute(`INSERT INTO myusers2(id,name,age) VALUES (1, 'Ravi', 23)`);
-    // let result = await connection.execute(`SELECT * FROM myusers2 where age = 23`);
-    res.send(result);
+    // let result = await connection.execute(`CREATE TABLE myusers5 (id number, name varchar2(20), age number)`);
+    // let result1 = await connection.execute(`INSERT INTO myusers("ID","name","AGE") VALUES (29, 'Bijay shah', 38)`);
 
+    const sqlQuery = `INSERT INTO myusers VALUES (:1, :2, :3)`;
+
+    binds = [ 
+      [34, "debashish", 29 ], 
+      [35, "sebastian", 39 ], 
+      [36, "siddharth", 27 ], 
+];
+    
+    result = await connection.executeMany(sqlQuery, binds, {});
+
+
+    let result2 = await connection.execute(`SELECT * FROM myusers`);
+    // await connection.close();
+    res.send(result2);
   } catch (err) {
     console.error("err1",err);
   } finally {
